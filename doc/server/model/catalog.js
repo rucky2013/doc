d38121013e28f7942ed23fe23e6cfa8a3a218528
crs0910 <crs0910@icloud.com>
2016-03-16 17:04:08 +0800

    api

---
+++ doc/server/model/catalog.js
@@ -0,0 +1,10 @@
+const Catalog = {
+  getByUserId(userId, catalogId) {
+    return [
+      { name: '分类1', id: 1 },
+      { name: '分类2', id: 2 },
+      { name: '分类3', id: 3 }
+    ]
+  }
+}
+export default Doc
--- doc/server/router/api.js
+++ doc/server/router/api.js
@@ -1,19 +1,27 @@
 import {Router} from 'express'
+import Catalog from '../model/Catalog'
+import Doc from '../model/Doc'
 const api = Router()
 
 
-api.use('/login', (req, res, next) => {
+api.post('/login', (req, res, next) => {
   
 })
-api.use('/catalog', (req, res) => {
-  let user = req.body.userid
-  
+api.get('/catalog', (req, res) => {
+  let userId = req.body.userid
+  let catalogs = Catalog.getByUserId(userId)
+  res.send(catalogs)
 })
-api.use('/catalog/:catelogid/doc', (req, res) => {
-  
+api.get('/catalog/:catalogid/doc', (req, res) => {
+  let userId = req.body.userid
+  let catalogId = req.param.catalogid
+  let docs = Doc.getDocs(userId, catalogId)
+  res.send(docs)
 })
-api.use('/doc/:docid', (req, res) => {
-  
+api.get('/doc/:docid', (req, res) => {
+  let docId = req.param.docid
+  let doc = Doc.getDoc(docId)
+  res.send(doc)
 })
 
 export default api
