864785efc42113eacb95495107b1de25bd2eb22d
crs0910 <crs0910@icloud.com>
2016-03-17 09:09:10 +0800

    ok

--- doc/client/build/webpack.base.conf.js
+++ doc/client/build/webpack.base.conf.js
@@ -69,14 +69,6 @@ module.exports = {
           limit: 10000,
           name: '[name].[ext]?[hash:7]'
         }
-      },
-      {
-        test: /\.less$/,
-        loader:'style!css!less'
-      },
-      {
-        test: /\.css$/,
-        loader:'style!css'
       }
     ]
   },
