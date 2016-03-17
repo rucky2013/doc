f6b73e748cf76d721750a57bf47c1bd453218b70
swarosky44 <495177660@qq.com>
2016-03-17 14:26:47 +0800

    ls update

--- doc/client/src/App.vue
+++ doc/client/src/App.vue
@@ -1,60 +1,28 @@
 <template>
-  <div class="g-test">
-    <div class="m-test"></div>
-    <button @click="test">提交</button>
-  </div>
+  <util-bar></util-bar>
+  <catalog :status="status"></catalog>
 </template>
 
 <script>
 require('normalize.css')
+require('../static/style/global.less')
+import utilBar from './components/utilBar'
+import catalog from './components/catalog'
 
 export default {
-  methods: {
-    test: function () {
-      console.log('come in !')
-      this
-      .$http.get('/api/v1/test')
-      .then((res) => {
-        console.log(res.data)
-      },
-      (err) => {
-        console.log(err)
-      })
+  data () {
+    return {
+      status: false
     }
+  },
+  computed: {
+    test () {
+      return this.status
+    }
+  },
+  components: {
+    'util-bar': utilBar,
+    'catalog': catalog
   }
 }
 </script>
-
-<style lang="less">
-html {
-  height: 100%;
-}
-
-body {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-
-#app {
-  margin-top: -100px;
-  max-width: 600px;
-  font-family: Helvetica, sans-serif;
-  text-align: center;
-}
-
-.logo {
-  width: 100px;
-  height: 100px
-}
-
-.g-test{
-  width: 100px;
-  height: 100px;
-  .m-test{
-    width: 50px;
-    height: 50px;
-  }
-}
-</style>
---
+++ doc/client/src/components/catalog.vue
@@ -0,0 +1,61 @@
+<template>
+  <div v-show="status" transition="expand" class="g-type">
+    <div class="m-tit">
+      类别
+    </div>
+    <ul class="m-body">
+      <li>类别一</li>
+      <li>类别二</li>
+      <li>类别三</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['status']
+  }
+</script>
+
+<style lang="less" scoped>
+  .g-type{
+    position: fixed;
+    top: 0;
+    height: 100%;
+    background: #ececec;
+    padding: 10px 20px;
+    .m-tit{
+      height: 80px;
+      line-height: 80px;
+      font-size: 1rem;
+      text-align: center;
+      border-bottom: 3px solid #FFF;
+      padding-bottom: 10px;
+    }
+    .m-body{
+      margin-top: 10px;
+      overflow-y: scroll;
+      li{
+        width: 100px;
+        text-align: center;
+        padding: 10px 0;
+        font-style: italic;
+        border-bottom: 2px solid #FFF;
+      }
+      li:hover{
+        background: #FF4D61;
+        color: #fff;
+        border-bottom: none;
+      }
+    }
+  }
+  .expand-transition{
+    left: 118px;
+    opacity: 1;
+    transition: all .3s ease;
+  }
+  .expand-enter, .expand-leave{
+    opacity: 0;
+    left:0px;
+  }
+</style>
